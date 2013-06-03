fad22c98cde247120e65bb949b69092523e90292
Scott Burton <scottburton11@gmail.com>
2013-06-03 12:26:20 -0700

    fixed redis loading for remote server

--- lib/server/config/server.rb
+++ lib/server/config/server.rb
@@ -1,8 +1,11 @@
+require 'bundler/setup'
+require 'em-redis'
+
 config['test'] = "Foo"
 
 if ENV["REDISTOGO_URL"]
   uri = URI.parse(ENV["REDISTOGO_URL"])
-  config['redis'] = EM::Protocols::Redis.connect.new(:host => uri.host, :port => uri.port, :password => uri.password)
+  config['redis'] = EM::Protocols::Redis.connect(:host => uri.host, :port => uri.port, :password => uri.password, :user => uri.user)
 else
   config['redis'] = EM::Protocols::Redis.connect 
 end
